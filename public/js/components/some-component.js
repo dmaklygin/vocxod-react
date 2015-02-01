d1d9b3391489685d6a368b64663f5832ea80119a
Dmitry Maklygin <d.maklygin@gmail.com>
2015-02-01 16:35:44 +0300

    example component

---
+++ public/js/components/some-component.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var SomeComponent = React.createClass({
+  render: function () {
+    return (
+      <div>Somethin SomeComponent</div>
+    )
+  }
+});
+
+module.exports = SomeComponent;
